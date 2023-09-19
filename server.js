782d96ee0633c9f14640961a10a52eb8f5ba8a44
Bitzilla <lucaraneri@hotmail.com>
2023-09-19 21:35:17 +0200

    fix: update to OpenAI SDK v4

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // imports
 import * as dotenv from "dotenv";
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai"; // OpenAI NodeJS SDK v4
 import express from "express";
 import cors from "cors";
 
@@ -8,12 +8,10 @@ import cors from "cors";
 dotenv.config();
 
 // create a configuration Object that requires the API Key
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI,
-})
-
 // initialize OpenAI SDK
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI,
+});
 
 // express server
 const app = express();
@@ -26,13 +24,22 @@ app.use(express.json()); // only json format requests
 app.post("/dream", async (req, res) => {
     const prompt = req.body.prompt;
     
-    const aiResponse = await openai.createImage({
-        prompt,
-        n: 1,
-        size: "1024x1024",
-    });
-    const image = aiResponse.data.data[0].url;
-    res.send({ image }); // send image back to client
+    try {
+        const aiResponse = await openai.createImage({
+            prompt,
+            n: 1,
+            size: "1024x1024",
+        });
+        const image = aiResponse.data.data[0].url;
+        res.send({ image }); // send image back to client
+    } catch (error) { // error handling
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+        } else {
+            console.log(error.message);
+        }
+    }
 });
 
 // start up server
