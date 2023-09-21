a834948cb06adf2dac3d0e66cd8e5a3ac99d265b
Bitzilla <lucaraneri@hotmail.com>
2023-09-21 14:20:42 +0200

    feat: loading spinner & code finalization

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-import './style.css'
+import './style.css';
 
 const form = document.querySelector("form");
 
 form.addEventListener("submit", async(e) => {
     e.preventDefault();
-    const data = FormData(form);
-    
+    showSpinner();
+    const data = new FormData(form);
+    console.log(data);
     const response = await fetch("http://localhost:8080/dream", {
         method: "POST",
         headers: {
@@ -16,7 +17,27 @@ form.addEventListener("submit", async(e) => {
         }),
     });
     
-    const { image } = await response.json();
-    const result = document.querySelector("#results");
-    result.innerHTML = `<img src="${image}" width="512" />`;
+    if(response.ok) {
+        const { image } = await response.json();
+        const result = document.querySelector("#results");
+        result.innerHTML = `<img src="${image}" width="512" />`; 
+    } else {
+        const err = await response.text();
+        alert(err);
+        console.error(err);
+    }
+    
+    hideSpinner();
 });
+
+function showSpinner() {
+    const button = document.querySelector("button");
+    button.disabled = true;
+    button.innerHTML = `Generating... <img class="spinner" src="./loading.png">`;
+}
+
+function hideSpinner() {
+    const button = document.querySelector("button");
+    button.disabled = false;
+    button.innerHTML = "Generate";
+}
